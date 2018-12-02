2cbf4e89dea6c4a0ab3aa94631d1ae07dc0b093d
Ignacio Machuca <ignacio.machuca@ug.uchile.cl>
2018-12-02 15:17:42 -0300

    Plot results

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from src.genetic_algorithm import GA
 from src.nqueens import fit as nqueens_fit
+import matplotlib.pyplot as plt
+
 
 MUTATION_PROB = 0.05
 POPULATION_SIZE = 100
-SEQUENCE_SIZE = 25  # length of the sequences
+SEQUENCE_SIZE = 25  # length of the sequences (or number of slots in a chess board)
 VOCABULARY = ['0', '1']  # possible values for a sequence
-GENERATIONS = 2000
+GENERATIONS = 500
 TOURNAMENT_SIZE = 10
 
 
@@ -23,7 +25,7 @@ if __name__ == "__main__":
 
     ga = GA(POPULATION_SIZE, SEQUENCE_SIZE, VOCABULARY, nqueens_fit, MUTATION_PROB)
     ga.get_initial_population()
-    population_fit.append(ga.avg_fitness())
+    #population_fit.append(ga.avg_fitness())
 
     for generation in range(GENERATIONS):
         ga.build_mating_pool(TOURNAMENT_SIZE)
@@ -32,4 +34,10 @@ if __name__ == "__main__":
         if first_solution_gen is None and ga.has_solution():
             first_solution_gen = generation
 
+    plt.plot(range(GENERATIONS), population_fit)
+    plt.xlabel("generation")
+    plt.ylabel("average population fitness")
+    if first_solution_gen:
+        plt.text(GENERATIONS/2, min(population_fit), "first solution\nfound @ {} generation".format(first_solution_gen))
+    plt.show()
     print(population_fit, first_solution_gen)
